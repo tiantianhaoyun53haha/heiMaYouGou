345144eb4a425761e1ede9564b833a0a30e1adea
xiongtian <18771777589@163.com>
2019-08-02 21:18:41 +0800

    2.4 Home page _ category list section completed

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,26 +2,42 @@
 
 
 Page({
-  data:{
+  data: {
     swiperList: [],
+    // 分类导航的数组
+    navCateList:[],
+
   },
   // 页面开始加载的时候触发
-  onLoad(){
+  onLoad() {
     this.getSwiperList();
- 
+    this.getNavCateList();
+
   },
   // 获取轮播图数据
-  getSwiperList(){
-   wx.request({
+  getSwiperList() {
+    wx.request({
       url: 'https://apis.zbztb.cn/api/public/v1/home/swiperdata',
-      success: (result)=>{
-    //  console.log(result.data)
-    // console.log(result.data.message)
-    this.setData({
-      swiperList:result.data.message,
-    })
-    console.log(this.data.swiperList)
+      success: (result) => {
+        //  console.log(result.data)
+        // console.log(result.data.message)
+        this.setData({
+          swiperList: result.data.message,
+        })
+        console.log(this.data.swiperList)
       },
+    });
+  },
+  getNavCateList() {
+    wx.request({
+      url: 'https://api.zbztb.cn/api/public/v1/home/catitems',
+      success: (result) => {
+        console.log(result),
+        this.setData({
+          navCateList: result.data.message
+        })
+      },
+
     });
   }
 })
