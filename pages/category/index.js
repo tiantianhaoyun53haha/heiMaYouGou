a82e763ae59ef428c4b59a32ffff64edbe0a7f59
xiongtian <18771777589@163.com>
2019-08-03 13:04:17 +0800

    2.8 The category list gets the data on the left and right

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,66 +1,50 @@
-// pages/category/index.js
+// 引入请求的函数
+import {request} from "../../request/request"
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+  // 存储数据的data
+  data:{
+    leftMenuList:[
+      {
+        cat_id:'',
+        cat_name:'',
+      }
+    ],
+    rightMenuList:[],
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  // 页面一启动，调用的函数
+  onLoad(){
+    this.getCategoryList()
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 发起分类商品数据请求的函数
+  getCategoryList(){
+    request({url: "/categories"})
+    .then((result=>{
+      // 左侧菜单栏需要的数据
+      console.log(result)
+      // 准备左边列表遍历的数据
+      // 这里用了map的高级用法，map(v=>({})),加括号的函数体返回对象字面表达式
+      const leftMenuList=result.map(v=>({
+      
+          // console.log(v.cat_id)
+          cat_id:v.cat_id,
+          cat_name:v.cat_name
+      
+      // }))
+      // const leftMenuList=result.map(v=>{
+      //   let cat_id=v.cat_id;
+      //   let cat_name=v.cat_name;
+      //   return  {cat_id,cat_name}
+      // })
+      // 准备右边列表需要的数据
+      const rightMenuList=result[0].children;
+      this.setData({
+        leftMenuList,
+        rightMenuList
+      })
+      console.log(this.data.leftMenuList)
+      console.log(this.data.rightMenuList)
+    }))
   }
 })
