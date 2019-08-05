aa66243acbf16634b0540b20a574f8787f252062
xiongtian <18771777589@163.com>
2019-08-05 14:47:42 +0800

    3.8 全局的正在等待的图标效果

--- request/request.js
+++ request/request.js
@@ -1,5 +1,11 @@
+// 同时发送的ajax的次数
+let ajaxTimes = 0;
+
 export const request = (params) => {
 // 公共的接口前缀
+ajaxTimes++;
+// 显示真正等待图标
+wx.showLoading({ title: "加载中" });
 const baseURL="https://api.zbztb.cn/api/public/v1";
 
     return new Promise((resolve, reject) => {
@@ -13,6 +19,15 @@ const baseURL="https://api.zbztb.cn/api/public/v1";
             fail: (err) => {
                 reject(err)
             },
+            // 请求完成的函数
+            complete:()=>{
+                ajaxTimes--;
+                if(ajaxTimes===0){
+                    // 最后一个请求回来了
+                    // 关闭正在等待的图标
+                    wx.hideLoading();
+                }
+            }
         });
     })
 }
