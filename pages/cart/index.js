0fa1edae544f67d4d7754351dc618d30e61a2e83
xiongtian <18771777589@163.com>
2019-08-08 17:20:58 +0800

    购物车页面 走通了获取收货地址的流程

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -1,66 +1,38 @@
 // pages/cart/index.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleChooseAddress(){
+    // 1.获取用户的授权信息
+   wx.getSetting({
+     success: (result1)=>{
+      //  1.1 获取用户的授权状态
+      const scopeAddress=result1.authSetting["scope.address"];
+          // 1.2 判断用户的授权状态
+          if(scopeAddress===true||scopeAddress===undefined){
+            // 1.3 直接获取用户的收货信息
+            wx.chooseAddress({
+              success: (result2)=>{
+                console.log(result2);
+              },
+             
+            });
+          }else{
+            // 2.1 诱导用户打开授权页面
+            wx.openSetting({
+              success: (result3)=>{
+                // 2.2 直接调用获取收货地址
+                wx.chooseAddress({
+                  success: (result4)=>{
+                      console.log(result4)
+                  },
+            
+                });
+              },
+             
+            });
+          }
+     },
+   
+   });
   }
+
 })
