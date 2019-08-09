00e92021ee1353db3abd1df6db60a4dbb7def2e8
xiongtian <18771777589@163.com>
2019-08-09 20:30:23 +0800

    5.3 购物车 计算全选和总价格和总数量功能

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -6,19 +6,25 @@ Page({
   data: {
     address: {},
     cart:{},
+    // 全选状态
+    isAllChecked:false,
+    // 总价格
+    totalPrice:0,
+    // 总数量 勾选了的总数量
+    totalNum:0,
+    // 购物车有没有商品
+    hasGoods:false
   },
 
 
   onShow() {
     const address=wx.getStorageSync("address")||{};
     const cart=wx.getStorageSync("cart")||{};
-      
-
     this.setData({
       address,
       cart,
-        
-    })
+    });
+    this.setCart(cart);
   },
 
 
@@ -47,6 +53,30 @@ Page({
 
     }
   },
-  
+
+  // 得到底部的计算总价，计算总数量，全选的状态
+  setCart(cart){
+    // 1.先把cart对象转换为数组
+    let cartArr=Object.values(cart);
+    // 设置全选的初始值，在循环里面找到里面的特别值
+    let isAllChecked=true;
+    // 计算总的价格
+    let totalPrice=0;
+    // 计算要购买的商品的总数量
+    let totalNum=0;
+    // 对数组进行遍历
+    cartArr.forEach(v=>{
+      if(v.checked){
+        // 选中了
+        totalPrice +=v.num*v.goods_price;
+        totalNum +=v.num;
+      }else{
+        isAllChecked=false;
+      }
+    })
+
+    // 把值重置回data里面，让值在页面上显示
+    this.setData({ isAllChecked, totalPrice, totalNum })
+  }
 
 })
