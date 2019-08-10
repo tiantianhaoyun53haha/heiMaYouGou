bab42ab5904f0535942ad58f1c05fcb32412a187
xiongtian <18771777589@163.com>
2019-08-10 23:25:42 +0800

    5.6 支付页面 动态渲染

--- pages/pay/index.js
+++ pages/pay/index.js
@@ -1,66 +1,61 @@
-// pages/pay/index.js
+// pages/cart/index.js
+// 解决async报错的问题
+import regeneratorRuntime from '../../lib/runtime/runtime';
+import { showToast,  getSetting, openSetting, chooseAddress,showModal } from "../../utils/asyncWx"
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
+    address: {},
+    cart:{},
+   
+
+    // 总价格
+    totalPrice:0,
+    // 总数量 勾选了的总数量
+    totalNum:0,
+ 
 
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
 
+  onShow() {
+    const address=wx.getStorageSync("address")||{};
+    const cart=wx.getStorageSync("cart")||{};
+    this.setData({
+      address,
+      cart,
+    });
+    this.setCart(cart);
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
 
+
+  // 得到底部的计算总价，计算总数量，全选的状态
+  setCart(cart){
+    // 1.先把cart对象转换为数组
+    let cartArr=Object.values(cart);
+    // 设置全选的初始值，在循环里面找到里面的特别值
+    let isAllChecked=true;
+    // 计算总的价格
+    let totalPrice=0;
+    // 计算要购买的商品的总数量
+    let totalNum=0;
+    // 对数组进行遍历
+    cartArr.forEach(v=>{
+      if(v.checked){
+        // 选中了
+        totalPrice +=v.num*v.goods_price;
+        totalNum +=v.num;
+      }else{
+        isAllChecked=false;
+      }
+    })
+    
+    // 判断购物车内有没有商品
+    const hasGoods=cartArr.length?true:false;
+    // 把值重置回data里面，让值在页面上显示
+    this.setData({hasGoods, cart,isAllChecked, totalPrice, totalNum })
+    wx.setStorageSync('cart', cart);
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
 
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
