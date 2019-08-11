e1463fb43494821883a266442a1046e660469a1e
xiongtian <18771777589@163.com>
2019-08-11 23:19:57 +0800

    6.7 订单页面 引入tabs组件

--- pages/order/index.js
+++ pages/order/index.js
@@ -1,66 +1,24 @@
 // pages/order/index.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    tabs: [
+      { id: 0, title: "全部", isActive: true },
+      { id: 1, title: "待付款", isActive: false },
+      { id: 2, title: "待发货", isActive: false },
+      { id: 3, title: "退款/退货", isActive: false }
+    ],
+    orderList: []
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+  handleItemChange(e) {
+    // 获取传递过来的索引
+    const { index } = e.detail;
+    // 获取tabs数组
+    let { tabs } = this.data;
+    // 循环修改tabs数组
+    tabs.forEach((v, i) => i === index ? v.isActive = true : v.isActive = false);
+    this.setData({ tabs });
+},
 
-  },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
