3e4e74cc1e0426a154d3f8f50b3073642f3ab93c
xiongtian <18771777589@163.com>
2019-08-13 12:37:29 +0800

    7.5 搜索中心  防抖

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,66 +1,52 @@
 // pages/search/index.js
+import regeneratorRuntime from '../../lib/runtime/runtime';
+import { request } from "../../request/request.js";
+
+
+
 Page({
 
-  /**
-   * 页面的初始数据
-   */
+
   data: {
+    inputValue: "",
+
+
+  },
+  // 定时器id
+
+
+
+
+
+  // input输入框的触发事件
+  handeSearchInput(e) {
+    // 输入框的值
+    TimeId: -1;
+    const { value } = e.detail;
+    // 简单的做一些验证
+    if (!value.trim()) {
+      this.setData({
+        goods: [],
+        inputValue: ""
+
+      })
+      return;
+    };
+
+    // 正常，防抖技术
+    clearTimeout(this.TimeId);
+    this.TimeId = setTimeout(() => {
+      this.getQSearch(value);
+
+    }, 1000);
 
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
 
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  async getQSearch(query){
+    const goods=await request({ url: "/goods/qsearch", data: { query } })
+    this.setData({goods});
   }
+
+
 })
